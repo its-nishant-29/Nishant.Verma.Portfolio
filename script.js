052db8b6e4298caf92f38a454b7edd1dc2cfc197
Nishant Verma <nishantverma2105@gmail.com>
2023-07-24 16:55:52 +0530

    updates

--- script.js
+++ script.js
@@ -16,17 +16,13 @@ $(document).ready(function () {
             
             $("html").css("scrollBehavior", "auto");
           });
-        
           $(".navbar .menu li a").click(function () {
-            
             $("html").css("scrollBehavior", "smooth");
           });
-
           $(".menu-btn").click(function () {
             $(".navbar .menu").toggleClass("active");
             $(".menu-btn i").toggleClass("active");
           });
-
           var typed = new Typed(".typing", {
             strings: [
               "Fullstack Developer",
